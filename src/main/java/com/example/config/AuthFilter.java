2554fdc5067f778aef9d344465d5ff0c54e30ca8
zyh <1653520397@qq.com>
2023-08-04 22:38:40 +0800

    system

--- src/main/java/com/example/config/AuthFilter.java
+++ src/main/java/com/example/config/AuthFilter.java
@@ -1,4 +1,41 @@
 package com.example.config;
 
-public class AuthFilter {
+import com.example.util.jwt;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+
+@WebFilter("/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException{
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterchain) throws IOException, ServletException{
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        System.out.println(request.getRequestURL());
+        String requestURI = request.getRequestURI();
+        if("/user/login".equals(requestURI) || "/user/register".equals(requestURI)){
+            filterChain.doFilter(servletRequest, servletResponse);
+
+        }else{
+            String token = request.getHeader("Authorization");
+            boolean b = jwt.parseToken(token);
+            if(b){
+                filterChain.doFilter(servletRequest, servletResponse);
+            }else{
+                servletResponse.getWriter().println("403");
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
